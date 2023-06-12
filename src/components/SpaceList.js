5c584199a1b29877eb83fa6b41a3510b47e746b9
Daniel <danoladesign@gmail.com>
2023-06-12 16:22:38 -0400

    updates date

--- src/components/SpaceList.js
+++ src/components/SpaceList.js
@@ -2,14 +2,13 @@ import React from 'react'
 import Planet from './Planet'
 import styles from './SpaceList.module.css'
 const SpaceList = ({planets}) => {
-    // console.log(planets)
   return (
       <ul className={styles['space-list']}>
           {planets.map(p => (
               <Planet 
               key={p.id}
               title={p.title}
-              date={`${p.date.slice(5, )}-2022`}
+              date={`${p.date.slice(5, )}-2023`}
               description={p.description}
               media={p.media}
               url={p.url}
