7eb41e8935024cc853ac2d2b4b28e9ac4bb5b86e
Daniel <danoladesign@gmail.com>
2023-06-12 23:49:03 -0400

    make year dynamic

--- src/components/SpaceList.js
+++ src/components/SpaceList.js
@@ -3,13 +3,15 @@ import Planet from './Planet'
 import styles from './SpaceList.module.css'
 
 const SpaceList = ({planets}) => {
-  return (
-      <ul className={styles['space-list']}>
+    const year = new Date().getFullYear()
+    console.log(year)
+    return (
+        <ul className={styles['space-list']}>
           {planets.map(p => (
               <Planet 
               key={p.id}
               title={p.title}
-              date={`${p.date.slice(5, )}-2023`}
+              date={`${p.date.slice(5, )}-${year}`}
               description={p.description}
               media={p.media}
               url={p.url}
